1c6bb8c4af066e313f38e2201e60dd70a50541ab
theautomation <github@theautomation.nl>
2024-11-07 22:10:52 +0100

    Downgrade mass

--- src/config/custom_components/mass/intent.py
+++ src/config/custom_components/mass/intent.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import json
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
 import voluptuous as vol
 from homeassistant.components.conversation import ATTR_AGENT_ID, ATTR_TEXT
@@ -14,17 +14,12 @@ from homeassistant.components.conversation.const import DOMAIN as CONVERSATION_D
 from homeassistant.config_entries import ConfigEntry, ConfigEntryState
 from homeassistant.core import HomeAssistant, State
 from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers import intent
-from music_assistant_models.enums import MediaType
-from music_assistant_models.errors import MusicAssistantError
+from music_assistant.common.models.errors import MusicAssistantError
 
 from . import DOMAIN
-from .const import ATTR_MASS_PLAYER_TYPE, CONF_OPENAI_AGENT_ID
-from .media_player import ATTR_MEDIA_ID, ATTR_MEDIA_TYPE, ATTR_RADIO_MODE
-
-if TYPE_CHECKING:
-    from . import MusicAssistantConfigEntry
+from .const import CONF_OPENAI_AGENT_ID
+from .media_player import ATTR_MEDIA_ID, ATTR_MEDIA_TYPE, ATTR_RADIO_MODE, MassPlayer
 
 INTENT_PLAY_MEDIA_ON_MEDIA_PLAYER = "MassPlayMediaOnMediaPlayer"
 NAME_SLOT = "name"
@@ -71,19 +66,18 @@ class MassPlayMediaOnMediaPlayerHandler(intent.IntentHandler):
         # pylint: disable=too-many-locals
         response = intent_obj.create_response()
         slots = self.async_validate_slots(intent_obj.slots)
-        config_entry: MusicAssistantConfigEntry
         config_entry = await self._get_loaded_config_entry(intent_obj.hass)
-        mass = config_entry.runtime_data.mass
-        mass_player_id = await self._async_get_matched_mass_player(
+
+        mass_player = await self._async_get_matched_mass_player(
             intent_obj, slots, config_entry
         )
+
         query = slots.get(QUERY_SLOT, {}).get(SLOT_VALUE)
         radio_mode = False
         if query:
             if not config_entry.data.get(CONF_OPENAI_AGENT_ID):
                 raise intent.IntentHandleError(
-                    "query requires using a conversation agent "
-                    "https://music-assistant.io/integration/voice/#ma-specific-conversation-agent"
+                    "query requires using a conversation agent https://music-assistant.io/integration/voice/#ma-specific-conversation-agent"
                 )
             ai_response = await self._async_query_ai(intent_obj, query, config_entry)
             try:
@@ -94,48 +88,35 @@ class MassPlayMediaOnMediaPlayerHandler(intent.IntentHandler):
                 return response
             media_id = json_payload.get(ATTR_MEDIA_ID)
             media_type = json_payload.get(ATTR_MEDIA_TYPE)
-            media_items = []
-            if isinstance(media_id, list):
-                media_items = [
-                    (
-                        await mass.music.get_item_by_name(
-                            item, media_type=MediaType(media_type)
-                        )
-                    ).to_dict()
-                    for item in media_id
-                ]
-                media_item = media_items
-            else:
-                media_item = await mass.music.get_item_by_name(
-                    media_id, media_type=MediaType(media_type)
-                )
             radio_mode = json_payload.get(ATTR_RADIO_MODE, False)
         else:
             artist = slots.get(ARTIST_SLOT, {}).get(SLOT_VALUE, "")
             track = slots.get(TRACK_SLOT, {}).get(SLOT_VALUE, "")
             album = slots.get(ALBUM_SLOT, {}).get(SLOT_VALUE, "")
             if track:
-                media_item = await mass.music.get_item_by_name(
-                    track, artist=artist, album=album, media_type=MediaType.TRACK
-                )
+                media_type = "track"
+                if artist:
+                    media_id = f"{artist} - {track}"
+                else:
+                    media_id = track
             elif album:
-                media_item = await mass.music.get_item_by_name(
-                    album, artist=artist, media_type=MediaType.ALBUM
-                )
+                media_type = "album"
+                if artist:
+                    media_id = f"{artist} - {album}"
+                else:
+                    media_id = album
             elif artist:
-                media_item = await mass.music.get_item_by_name(
-                    artist, artist=artist, album=album, media_type=MediaType.ARTIST
-                )
+                media_type = "artist"
+                media_id = artist
             else:
                 raise intent.IntentHandleError("No media item parsed from query")
 
         try:
-            await mass.player_queues.play_media(
-                queue_id=mass_player_id,
-                media=(
-                    media_item if isinstance(media_item, list) else media_item.to_dict()
-                ),
-                radio_mode=radio_mode,
+            await mass_player.async_play_media(
+                media_type=media_type,
+                media_id=media_id,
+                enqueue=None,
+                extra={ATTR_RADIO_MODE: radio_mode},
             )
         except MusicAssistantError as err:
             raise intent.IntentHandleError(err.args[0] if err.args else "") from err
@@ -149,19 +130,22 @@ class MassPlayMediaOnMediaPlayerHandler(intent.IntentHandler):
         intent_obj: intent.Intent,
         slots: intent._SlotsType,
         config_entry: ConfigEntry,
-    ) -> str:
+    ) -> MassPlayer:
         name: str | None = slots.get(NAME_SLOT, {}).get(SLOT_VALUE)
         if name == "all":
             # Don't match on name if targeting all entities
             name = None
         area_name = slots.get(AREA_SLOT, {}).get(SLOT_VALUE)
         state = await self._get_matched_state(intent_obj, name, area_name)
-        entity_registry = er.async_get(self.hass)
-        if entity := entity_registry.async_get(state.entity_id):
-            return entity.unique_id.split("mass_", 1)[1]
-        raise intent.IntentHandleError(
-            f"No entities matched for: name={name}, area_name={area_name}"
+        mass_player = MassPlayer(
+            intent_obj.hass.data[DOMAIN][config_entry.entry_id].mass,
+            state.attributes.get("mass_player_id"),
         )
+        if mass_player is None:
+            raise intent.IntentHandleError(
+                f"No entities matched for: name={name}, area_name={area_name}"
+            )
+        return mass_player
 
     async def _async_query_ai(
         self, intent_obj: intent.Intent, query: str, config_entry: ConfigEntry
@@ -193,8 +177,9 @@ class MassPlayMediaOnMediaPlayerHandler(intent.IntentHandler):
         mass_states: set[str] = set()
         initial_states = intent_obj.hass.states.async_all()
         for state in initial_states:
-            if state.attributes.get(ATTR_MASS_PLAYER_TYPE) is not None:
+            if state.attributes.get("mass_player_id") is not None:
                 mass_states.add(state)
+
         states = list(
             intent.async_match_states(
                 intent_obj.hass,
@@ -203,12 +188,15 @@ class MassPlayMediaOnMediaPlayerHandler(intent.IntentHandler):
                 states=mass_states,
             )
         )
+
         if not states:
             raise intent.IntentHandleError(
                 f"No entities matched for: name={name}, area_name={area_name}"
             )
+
         if len(states) > 1:
             raise intent.IntentHandleError(
                 f"Multiple entities matched for: name={name}, area_name={area_name}"
             )
+
         return states[0]
